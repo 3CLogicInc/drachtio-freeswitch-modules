a8778389cfe6c2da82443ea7ccd045f18eb9e204
Asad Khan <akhan@3clogic.com>
2024-02-19 10:28:34 +0530

    changes

--- modules/mod_google_transcribe/mod_google_transcribe.h
+++ modules/mod_google_transcribe/mod_google_transcribe.h
@@ -18,7 +18,7 @@
 #define TRANSCRIBE_EVENT_PLAY_INTERRUPT "google_transcribe::play_interrupt"
 
 #define TRANSCRIBE_EVENT_RESULTS_2 "google_transcribe::transcriptionapi"
-#define TRANSCRIBE_EVENT_END_OF_UTTERANCE_2 "google_transcribe::end_of_utterance2api"
+#define TRANSCRIBE_EVENT_END_OF_UTTERANCE_2 "google_transcribe::end_of_utteranceapi"
 #define TRANSCRIBE_EVENT_START_OF_TRANSCRIPT_2 "google_transcribe::start_of_transcriptapi"
 #define TRANSCRIBE_EVENT_END_OF_TRANSCRIPT_2 "google_transcribe::end_of_transcriptapi"
 #define TRANSCRIBE_EVENT_NO_AUDIO_DETECTED_2 "google_transcribe::no_audio_detectedapi"
